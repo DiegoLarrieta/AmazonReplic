d6e8b69ff219039301e52f156e832a82fef7e387
diego <diegolarrieta@gmail.com>
2024-06-26 15:14:55 -0600

    generate html order summary

--- js/amazon.js
+++ js/amazon.js
@@ -1,4 +1,6 @@
-import { cart as myCart} from "../data/cart.js";
+import { cart , addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 let productsHtml = '';
 
 products.forEach((product)=>{
@@ -60,40 +62,24 @@ products.forEach((product)=>{
 document.querySelector('.js-products-grid').innerHTML = productsHtml;
 const addedMessageTimeouts = {};
 
+
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem)=>{
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button)=>{
     button.addEventListener('click',()=>{
       const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
 
-      let matchingItem;
-
-      cart.forEach((item)=>{
-        if(productId === item.productId){
-          matchingItem = item;
-        }
-      });
-
-      const quantitySelector = document.querySelector(
-        `.js-quantity-selector-${productId}`
-      );
-
-      const quantity = Number(quantitySelector.value);
-
-      if(matchingItem){
-        matchingItem.quantity += quantity;
-      }else{
-        cart.push({
-          productId : productId,
-          quantity : quantity
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((item)=>{
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+      // document.querySelector('.js-cart-quantity')
+      //   .innerHTML = cartQuantity;
 
         const addedMessage = document.querySelector(
           `.js-added-to-cart-${productId}`
