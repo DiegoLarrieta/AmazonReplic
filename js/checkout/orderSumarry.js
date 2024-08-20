62837f4895242f1a3804cd31a7559b61b34c08dc
diego <diegolarrieta@gmail.com>
2024-08-20 11:02:29 -0600

    test passed

--- js/checkout/orderSumarry.js
+++ js/checkout/orderSumarry.js
@@ -31,6 +31,7 @@ import { renderPaymentSummary } from "./paymentSumarry.js";
 
     cartSummaryHTML += `
       <div class="cart-item-container
+        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
@@ -47,7 +48,7 @@ import { renderPaymentSummary } from "./paymentSumarry.js";
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
               </span>
@@ -60,7 +61,9 @@ import { renderPaymentSummary } from "./paymentSumarry.js";
                 data-product-id="${matchingProduct.id}">
                 Save
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link
+                js-delete-link-${matchingProduct.id}" 
+                data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
